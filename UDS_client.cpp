b1ad5b8925f06101c0db3df078d5c7834c21ca49
yvishalr <yellurvishalrao@gmail.com>
2023-01-04 13:00:52 +0530

    DID length fixed. NRC functionality implemented.

--- UDS_client.cpp
+++ UDS_client.cpp
@@ -6,7 +6,7 @@ uint8_t PCI;
 
 /* data parameter definitions for with DID and with subfunction respectively */
 uint64_t DATA;
-uint8_t DATA_ARRAY[5] = {0x23, 0x48, 0x55, 0x47};
+uint8_t DATA_ARRAY[5] = {0xAA, 0xBB, 0xCC, 0xDD};
 
 /* sub function definitions */
 uint8_t suppressPosRspMsgIndicationBit = 0;
@@ -17,6 +17,7 @@ uint8_t MODE;
 uint8_t BLOCKSIZE = 0;
 uint8_t ST = 0;
 
+/* Method prototypes */
 void fileWrite();
 
 /* MAIN */
@@ -26,10 +27,12 @@ int main(void)
     f[0] = f[1] = f[2] = f[3] = f[4] = f[5] = f[6] = f[7] = 0xAA;
 
     // example single frame with a subfunction
-    PCI = 0x6;
+    PCI = 0x3;
     SUBFUNCTION = SID->subf.sub_4 | (suppressPosRspMsgIndicationBit << 7);
-    single_frame_subf(f, PCI, SID->WDBI, SUBFUNCTION, DATA_ARRAY);
+    single_frame_did(f, PCI, SID->RDBI, 0x1403, 0x00);
     display_frame(f);
+
+    // imitate sending frame into bus
     fileWrite();
 
     return 0;
--- UDS_server.cpp
+++ UDS_server.cpp
@@ -4,6 +4,11 @@
 /* Define all valid SIDs for example script */
 uint8_t stored_SID[] = {0x22, 0x3E, 0x2E, 0x10, 0x11};
 
+/* All valid DIDs with corresponding virtual register values for example script */
+uint16_t stored_DID[] = {0x1401, 0x1402, 0x1403, 0x1404, 0x1405};
+uint8_t register_data[] = {0x1F, 0x2F, 0x3F, 0x4F, 0x5F};
+
+/* Method prototypes */
 void readFile();
 
 /* MAIN */
@@ -12,30 +17,57 @@ int main(void)
     // imitate reading bus data
     readFile();
 
-    short flag = 0;
+    short flagS = 0, flagD = 0;
     for (short i = 0; i < 5; i++)
     {
         if (stored_SID[i] == f[1])
-            flag = 1;
+        {
+            flagS = 1;
+            break;
+        }
     }
 
-    if (flag)
+    if (flagS)
     {
-        printf("\nPositive Response :- \n");
+        uint16_t searchDID = (f[2] << 8) | f[3];
+        for (short i = 0; i < 5; i++)
+        {
+            if (stored_DID[i] == searchDID)
+            {
+                flagD = i + 1;
+                break;
+            }
+        }
 
-        f[0] = 0x4; // specify pci length here, assumed 4
-        f[1] = f[1] + 0x40;
-        f[3] = 0x12;
-        f[4] = 0x34;
+        if (flagD)
+        {
+            printf("\nPositive Response :- \n");
+
+            f[0] = 0x4; // specify pci length here, assumed 4
+            f[1] = f[1] + 0x40;
+            f[4] = register_data[flagD - 1];
+            f[5] = f[6] = f[7] = 0x00;
+        }
+        else
+        {
+            printf("\nNegative Response :- \n");
+
+            f[0] = 0x3;
+            f[2] = f[1];
+            f[1] = 0x7F;
+            f[3] = 0x31; // NRC (Request out of range)
+            f[4] = f[5] = f[6] = f[7] = 0x00;
+        }
     }
     else
     {
         printf("\nNegative Response :- \n");
 
         f[0] = 0x3;
+        f[2] = f[1];
         f[1] = 0x7F;
-        f[3] = 0x31; // assumed NRC
-        f[4] = f[5] = f[6] = f[7] = 0;
+        f[3] = 0x11; // NRC (Service not supported)
+        f[4] = f[5] = f[6] = f[7] = 0x00;
     }
 
     display_frame(f);
